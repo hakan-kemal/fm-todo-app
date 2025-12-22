af5a26233e4bd139d5950acf6f1d3ce427cafc14
Hakan Çuhadar <h.k.cuhadar@gmail.com>
2025-12-22 22:17:47 +0100

    add background image and todo functionality

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
 const itemsCount = document.getElementById('items-count');
 
-itemsCount.textContent = '4';
+let todo = [];
+
+todoInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    todo.push({ text: todoInput.value, completed: false });
+    todoInput.value = '';
+    todoList.innerHTML = '';
+
+    todo.forEach((item) => {
+      const li = document.createElement('li');
+      li.textContent = item.text;
+      todoList.appendChild(li);
+    });
+
+    itemsCount.textContent = todo.length.toString();
+  }
+});
